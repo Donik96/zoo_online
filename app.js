597b5e653aa6e031c7b8080408f1acf8bc010880
Donik96 <fazilovbora39@gmail.com>
2023-02-01 18:36:53 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+$('.slick-wrapper').slick({
+    slidesToShow:1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    centerMode: true,
+    centerPadding: '140px',
+    slidesToShow: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '60px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
