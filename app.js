593f4dd4299bded0820df591819ba44e9bb4a8d0
Donik96 <fazilovbora39@gmail.com>
2023-02-03 11:04:19 +0500

    git commit

--- app.js
+++ app.js
@@ -30,27 +30,7 @@ $('.slick-wrapper').slick({
 
 
   $('.slick2').slick({
-    centerMode: true,
-    centerPadding: '60px',
+    infinite: true,
     slidesToShow: 3,
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
-        }
-      }
-    ]
+    slidesToScroll: 3
   });
